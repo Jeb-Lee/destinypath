fc252f985d30113a42135782b98f60b08db52282
HAZEL KIM HYE JIN <kimhyejinaaaa@gmail.com>
2025-05-01 08:59:41 +0800

    update

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  plugins: [react()],
+  server: {
+    port: 3000,
+    host: true,
+    watch: {
+      usePolling: true
+    }
+  }
+});

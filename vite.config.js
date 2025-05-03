e0ad627696803908b38835d39488cfac12715a09
HAZEL KIM HYE JIN <kimhyejinaaaa@gmail.com>
2025-05-03 22:22:13 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite';
-   import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-   export default defineConfig({
-     plugins: [react()],
-     server: {
-       port: 3003,
-       open: true,
-       host: true
-     }
-   });
+export default defineConfig({
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+      babel: {
+        babelrc: false, // Explicitly ignore .babelrc files
+        configFile: false // Don't use babel.config.js
+      }
+    })
+  ],
+  esbuild: {
+    loader: 'jsx', // Force ESBuild to handle JSX
+  }
+})

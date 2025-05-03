cb5c0c0bce32f45bd9bac8cc30b3176c78a7d631
HAZEL KIM HYE JIN <kimhyejinaaaa@gmail.com>
2025-05-03 15:20:19 +0800

    update .babelrc

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+   import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
-  plugins: [react()],
-  server: {
-    port: 3000,
-    host: true,
-    watch: {
-      usePolling: true
-    }
-  }
-});
+   export default defineConfig({
+     plugins: [react()],
+     server: {
+       port: 3003,
+       open: true,
+       host: true
+     }
+   });
